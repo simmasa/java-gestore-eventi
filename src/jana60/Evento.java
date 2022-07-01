d0767d7f6d7b847631de2f294df6ae32ec91cc3f
simmasa <s.masa@hotmail.it>
2022-07-01 10:05:52 +0200

    milestone 2 completa

--- src/jana60/Evento.java
+++ src/jana60/Evento.java
@@ -1,4 +1,86 @@
 package jana60;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Evento {
+    private final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private String titolo;
+    private LocalDate data;
+    private int postiTot, postiPrenotati;
+
+    //contructor
+
+    public Evento(String titolo, String data, int postiTot) throws IllegalArgumentException {
+        validPosti(postiTot);
+
+        this.titolo = titolo;
+        this.data = validData(data);
+        this.postiTot = postiTot;
+        this.postiPrenotati = 0;
+    }
+
+    //getter/setter
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(String data) throws IllegalArgumentException {
+        this.data = validData(data);
+    }
+
+    public int getPostiTot() {
+        return postiTot;
+    }
+
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    //validator
+
+    private LocalDate validData(String data) throws IllegalArgumentException {
+        LocalDate date = LocalDate.parse(data,format);
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data non può essere nel passato");
+        }
+        return date;
+    }
+    private void validPosti(int posti) throws IllegalArgumentException {
+        if (posti<1)
+            throw new IllegalArgumentException("Ci deve essere almeno un posto");
+    }
+
+    //altri
+
+    public void prenota() throws Exception {
+        if (LocalDate.now().isAfter(data)) {
+            throw new Exception("Impossibile prenotare eventi già passati.");
+        } else if (postiTot==postiPrenotati) {
+            throw new Exception("Non ci sono abbastanza posti disponibili.");
+        } else
+            postiPrenotati++;
+    }
+    public void disdici() throws Exception {
+        if (LocalDate.now().isAfter(data)) {
+            throw new Exception("Impossibile disdire eventi già passati");
+        } else if (postiPrenotati<1) {
+            throw new Exception("Non ci sono prenotazioni per questo evento");
+        }else
+            postiPrenotati--;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento in data "+ format.format(data)+" dal titolo "+titolo;
+    }
 }
---
+++ src/jana60/Main.java
@@ -0,0 +1,60 @@
+package jana60;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Benvenuto");
+        System.out.println("Inserisci i dati del tuo evento:");
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Evento> listaEventi = new ArrayList<>();
+
+        String titolo,data;
+        int posti;
+
+        System.out.println("Inserisci il titolo dell'evento:");
+        titolo= "titolo";
+        System.out.println("Inserisci la data del tuo evento in formato dd/mm/yyyy : ");
+        data = "27/12/2022";
+
+        try {
+            System.out.println("Inserisci la quantità di posti a tua disposizione:");
+            posti = Integer.parseInt(scan.nextLine());
+
+            Evento primo = new Evento(titolo, data, posti);
+            listaEventi.add(primo);
+            System.out.println("Abbiamo aggiunto il tuo evento alla nostra lista");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String opt;
+        do {
+            System.out.println("Vuoi prenotare dei posti per l'evento? Premi '1' per confermare o '2' per uscire");
+            opt = scan.nextLine();
+        }while (!opt.equals("1") && !opt.equals("2"));
+        switch (opt) {
+            case "1":
+                int postiDaPrenotare = 0;
+                do {
+                    System.out.println("Quanti posti vuoi prenotare?");
+                    try {
+                        postiDaPrenotare = Integer.parseInt(scan.nextLine());
+                        for (int i =0; i <postiDaPrenotare;i++)
+                            listaEventi.get(0).prenota();
+                    } catch (NumberFormatException e) {
+                        System.out.println("Solo i numeri possono essere inseriti");
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                } while (postiDaPrenotare==0);
+                System.out.println("Grazie per aver prenotato dei posti.");
+                break;
+
+            case "2":
+                System.out.println("Grazie per aver creato un evento.");
+        }
+
+    }
+}
